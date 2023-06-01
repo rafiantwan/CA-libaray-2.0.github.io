b98f7b6538f9d6292492d860ad87a883b314fe86
rafiantwan <rafiantwan5@gmail.com>
2023-06-01 00:32:31 -0400

    nice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import './index.css';
-import Nav from './components/nav';
-import Landing from './components/Landing';
-import Highlights from './components/Highlights';
-import Featured from './components/Featured'
-import Discounted from './components/Discounted';
-import Explore from './components/Explore';
-import Footer from './components/Footer';
-
-
+import "./index.css";
+import Nav from "./components/nav";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" render={() => <Books books={books} />}/>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
